e1ec5836728d2ca2c2420361e2e19ffb9a55736e
Anlimah <y.m.ratty7@gmail.com>
2022-12-16 10:53:08 +0000

    Added spreadsheet generator 3: OOP

--- print-document.php
+++ print-document.php
@@ -4,11 +4,37 @@ require_once('bootstrap.php');
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Src\Controller\ExposeDataController;
 
-$spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'Hello World !');
+class Broadsheet
+{
+    private $spreadsheet = null;
+    private $writer = null;
 
-$writer = new Xlsx($spreadsheet);
-$writer->save('hello world.xlsx');
+    public function __construct()
+    {
+        $spreadsheet = new Spreadsheet();
+        $writer = new Xlsx($spreadsheet);
+    }
+
+    private function makeSpreadsheetContent()
+    {
+        $sheet = $this->spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Hello World !');
+    }
+
+    private function saveSpreadsheetFile()
+    {
+        $this->writer->save('hello world.xlsx');
+    }
+
+    public function generate()
+    {
+        $this->makeSpreadsheetContent();
+        $this->saveSpreadsheetFile();
+    }
+}
+
+$broadsheet = new Broadsheet();
+$broadsheet->generate();
 echo "OKAY";
