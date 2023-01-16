eadc0ecd796b9fdb56440ebf496dff5538059833
Anlimah <y.m.ratty7@gmail.com>
2023-01-16 16:11:56 +0000

    Working on awaiting page 1

--- awaiting-results.php
+++ awaiting-results.php
@@ -22,7 +22,7 @@ require_once('inc/page-data.php');
   <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Applications</h1>
+      <h1>Awaiting</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">Home</a></li>
@@ -43,25 +43,27 @@ require_once('inc/page-data.php');
               <div class="col-xxl-3 col-md-3" style="width: 500px">
                 <div class="card info-card">
                   <div class="card-body">
-                    <h5 class="card-title" style="text-align: center;">Upload Results Datasheet</h5>
+                    <h5 class="card-title" style="text-align: center;"> Download/Upload Awaiting Datasheet</h5>
                     <div style="display: flex; flex-direction:column; align-items: center; justify-content:center;">
                       <div id="data-upload-form">
                         <p id="upload-notification" class="text-success"></p>
+                        <label class="btn btn-warning me-4" id="downloadBtn" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Export current list of awaiting applications to an excel file"><i class="bi bi-download"></i> Download</label>
+                        <label for="awaiting-ds" class="btn btn-danger" id="uploadBtn" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Upload data of awaiting applications"><i class="bi bi-upload"></i> Upload</label>
                         <form id="upload-awaiting-form" action="" method="post">
-                          <label for="awaiting-ds" class="btn btn-primary" id="uploadBtn">Upload</label>
                           <input type="file" name="awaiting-ds" id="awaiting-ds" style="display: none;" accept=".xlsx,.xls,pplication/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel">
                           <input type="hidden" name="action" value="uad">
                           <input type="hidden" name="startRow" value="1">
                           <input type="hidden" name="endRow" value="0">
                         </form>
                       </div>
-                      <div id="data-process-info" class="mt-4" style="width:100% !important; display:flex; flex-direction:column; align-items:center">
+                      <div id="data-process-info" class="mt-4" style="display:none !important; width:100% !important; display:flex; flex-direction:column; align-items:center">
+                        <h5 class="text-center">Upload Summary</h5>
                         <ol class="list-group list-group-horizontal" style="font-size: 12px !important; font-family: Verdana, Arial, Tahoma, Serif !important;">
                           <li class="list-group-item d-flex justify-content-between align-items-start">
                             <div class="ms-2 me-2">
                               <div class="fw-bold">Total</div>
                             </div>
-                            <span class="badge bg-warning rounded-pill">14</span>
+                            <span class="badge bg-dark rounded-pill">14</span>
                           </li>
                           <li class="list-group-item d-flex justify-content-between align-items-start">
                             <div class="ms-2 me-2">
@@ -70,25 +72,51 @@ require_once('inc/page-data.php');
                             <span class="badge bg-success rounded-pill">14</span>
                           </li>
                           <li class="list-group-item d-flex justify-content-between align-items-start">
-                            <div class="ms-2 me-auto">
+                            <div class="ms-2 me-2">
                               <div class="fw-bold">Errors</div>
                             </div>
                             <span class="badge bg-danger rounded-pill">14</span>
                           </li>
                         </ol>
-                        <div class="error-info mt-4" style="font-size: 12px !important; font-family: Verdana, Arial, Tahoma, Serif !important;">
-                          <h3 class="text-danger">Errors Encountered</h3>
-                          <table>
+
+                        <div class="error-info mt-4" style="display:none; font-size: 11px !important; font-family: Verdana, Arial, Tahoma, Serif !important;">
+                          <h5 class="text-danger text-center">Errors Encountered</h5>
+                          <table class="table table-striped">
                             <thead>
                               <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Index Number</th>
-                                <th scope="col">Message</th>
+                                <th scope="col" colspan="1">#</th>
+                                <th scope="col" colspan="1">Index No.</th>
+                                <th scope="col" colspan="2">Error Message</th>
                               </tr>
                             </thead>
                             <tbody>
                               <tr>
                                 <td>1</td>
+                                <td>123456789</td>
+                                <td>Applicant index number doesn't match any record in database</td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td>123456789</td>
+                                <td>Applicant index number doesn't match any record in database</td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td>123456789</td>
+                                <td>Applicant index number doesn't match any record in database</td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td>123456789</td>
+                                <td>Applicant index number doesn't match any record in database</td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td>123456789</td>
+                                <td>Applicant index number doesn't match any record in database</td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
                                 <td>123456789</td>
                                 <td>Applicant index number doesn't match any record in database</td>
                               </tr>
@@ -108,72 +136,6 @@ require_once('inc/page-data.php');
               </div>
               <!-- End Admitted Students Card -->
             </div>
-          </div>
-
-          <div class="card recent-sales overflow-auto">
-
-            <div class="card-body" style="display: none;">
-              <h5 class="card-title">Admit Applicants</h5>
-              <form id="fetchDataForm" class="mb-4">
-                <div class="row">
-                  <div class="col-3">
-                    <label for="cert-type" class="form-label">Certificate Type</label>
-                    <select name="cert-type" id="cert-type" class="form-select">
-                      <option value="" hidden>Choose Certificate</option>
-                      <option value="WASSCE">WASSCE/NECO</option>
-                      <option value="SSSCE">SSSCE/GBCE</option>
-                      <option value="Baccalaureate">BACCALAUREATE</option>
-                      <option value="ALL">ALL</option>
-                    </select>
-                  </div>
-                  <div class="col-3">
-                    <label for="prog-type" class="form-label">Programme Category</label>
-                    <select name="prog-type" id="prog-type" class="form-select">
-                      <option value="" hidden>Choose Category</option>
-                      <option value="first_prog">First Choice</option>
-                      <option value="second_prog">Second Choice</option>
-                    </select>
-                  </div>
-                  <div class="col-2">
-                    <button type="submit" class="btn mb-4 btn-primary" style="margin-top: 30px;">Fetch Data</button>
-                  </div>
-                </div>
-              </form>
-              <div id="info-output"></div>
-              <table class="table table-borderless datatable table-striped table-hover">
-                <thead>
-                  <tr class="table-dark">
-                    <th scope="col">#</th>
-                    <th scope="col" colspan="1">Full Name</th>
-                    <th scope="col" colspan="1">Programme: (<span class="pro-choice">1<sup>st</sup></span>) Choice</th>
-                    <th scope="col" colspan="4" style="text-align: center;">Core Subjects</th>
-                    <th scope="col" colspan="4" style="text-align: center;">Elective Subjects</th>
-                    <th scope="col" colspan="1" style="text-align: center;">Status</th>
-                  </tr>
-                  <tr class="table-grey">
-                    <th scope="col"></th>
-                    <th scope="col"></th>
-                    <th scope="col"></th>
-                    <th scope="col" style="background-color: #999; text-align: center" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Core Mathematics">CM</th>
-                    <th scope="col" style="background-color: #999; text-align: center" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="English Language">EL</th>
-                    <th scope="col" style="background-color: #999; text-align: center" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Integrated Science">IS</th>
-                    <th scope="col" style="background-color: #999; text-align: center" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Social Studies">SS</th>
-                    <th scope="col" style="background-color: #999; text-align: center" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Elective 1">E1</th>
-                    <th scope="col" style="background-color: #999; text-align: center" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Elective 2">E2</th>
-                    <th scope="col" style="background-color: #999; text-align: center" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Elective 3">E3</th>
-                    <th scope="col" style="background-color: #999; text-align: center" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Elective 4">E4</th>
-                    <th scope="col"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                </tbody>
-              </table>
-              <div class="mt-4" style="float:right">
-                <button class="btn btn-primary" id="admit-all-bs">Admit All Qualified</button>
-              </div>
-              <div class="clearfix"></div>
-            </div>
-
           </div>
         </div><!-- End Recent Sales -->
 
