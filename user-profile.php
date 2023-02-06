16380d7cf1fe31e0ec587317bb71af8573c70b2e
Anlimah <y.m.ratty7@gmail.com>
2023-02-06 20:52:44 +0000

    Corrected user-profile errors

--- user-profile.php
+++ user-profile.php
@@ -38,13 +38,137 @@ require_once('inc/page-data.php');
 <html lang="en">
 
 <head>
-  <?= require_once("inc/head.php") ?>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Dashboard - Admissions</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/rmu-logo.png" rel="icon">
+  <link href="assets/img/rmu-logo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <!--<link href="https://fonts.gstatic.com" rel="preconnect">-->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <style>
+    .btn-group-xs>.btn,
+    .btn-xs {
+      padding: 1px 5px;
+      font-size: 12px;
+      line-height: 1.5;
+      border-radius: 3px;
+    }
+  </style>
+  <script src="js/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
-  <?= require_once("inc/header.php") ?>
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="index.php" class="logo d-flex align-items-center">
+        <img src="assets/img/rmu-logo.png" alt="">
+        <span class="d-none d-lg-block">RMU Admissions</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
 
-  <?= require_once("inc/sidebar.php") ?>
+    <nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
+
+        <li class="nav-item dropdown pe-3">
+
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <img src="assets/img/icons8-circled-user-male-skin-type-5-96.png" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2"><?= $admin->fetchUserName($_SESSION["user"])[0]["userName"] ?></span>
+          </a><!-- End Profile Iamge Icon -->
+
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <li class="dropdown-header">
+              <h6><?= $admin->fetchFullName($_SESSION["user"])[0]["fullName"] ?></h6>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="user-profile.php">
+                <i class="bi bi-person"></i>
+                <span>My Profile</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="?logout=true">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+              </a>
+            </li>
+
+          </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
+
+      </ul>
+    </nav><!-- End Icons Navigation -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="index.php">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <?php
+      if (strtolower($_SESSION["role"]) == "admissions") {
+      ?>
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="admissions/admit-applicants.php">
+            <i class="bi bi-files"></i>
+            <span>Admit Applicants</span>
+          </a>
+        </li><!-- End Broadsheet Page Nav -->
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="admissions/broadsheet.php">
+            <i class="bi bi-person-check"></i>
+            <span>Broadsheet</span>
+          </a>
+        </li><!-- End Application Page Nav -->
+      <?php }
+      if (strtolower($_SESSION["role"]) == "admissions" || strtolower($_SESSION["role"]) == "accounts") {
+      ?>
+        <li class="nav-item">
+          <a class="nav-link collapsed" href="admissions/user-account.php">
+            <i class="bi bi-shield-shaded"></i>
+            <span>User Account</span>
+          </a>
+        </li><!-- End User Account Page Nav -->
+      <?php } ?>
+    </ul>
+  </aside><!-- End Sidebar-->
 
   <main id="main" class="main">
 
