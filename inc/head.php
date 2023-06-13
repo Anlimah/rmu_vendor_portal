757868a1518d3dcda3d5aaa91e9d9421c5bd4d62
Anlimah <y.m.ratty7@gmail.com>
2023-06-13 20:03:00 +0000

    Corrected favicon link

--- inc/head.php
+++ inc/head.php
@@ -2,8 +2,6 @@
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
 <title>Dashboard - Admissions</title>
-<meta content="" name="description">
-<meta content="" name="keywords">
 
 <!-- Favicons -->
 <link href="../assets/img/rmu-logo.png" rel="icon">
